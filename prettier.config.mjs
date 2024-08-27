6bcf90d38cf2b996ad97a16c289364c3b23f4625
Pedro Gomes <pcgbernardes@gmail.com>
2024-08-27 11:12:56 -0400

    feat(conf):init package & add lint/style config

---
+++ prettier.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('Prettier').Config}*/
+export const config = {
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'es5',
+  arrowParens: 'always',
+  printWidth: 80,
+  useTabs: false,
+  tabWidth: 2,
+}
+
+export default config
