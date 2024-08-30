788840d138ded2418355d3066d8ea71f998db45b
Pedro Gomes <pcgbernardes@gmail.com>
2024-08-30 10:15:43 -0400

    chore(lint):remove unused rule & move to commonjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,12 +1,11 @@
 /**@type {import('eslint').Linter.Config} */
-export default {
+module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: true,
   },
   plugins: ['@typescript-eslint'],
   extends: [
-    'next/core-web-vitals',
     'plugin:@typescript-eslint/recommended-type-checked',
     'plugin:@typescript-eslint/stylistic-type-checked',
   ],
